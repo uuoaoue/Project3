6e45aa0e7d9cc9f8480adbec7add20b079c80546
uuoaoue <uyen.p.huynh@sjsu.edu>
2021-11-15 13:10:21 -0800

    add teapot to scene

---
+++ P1.js
@@ -0,0 +1,191 @@
+// Art 109 Three.js Demo Site
+// client7.js
+// A three.js scene which uses planes and texture loading to generate a scene with images which can be traversed with basic WASD and mouse controls, this scene is full screen with an overlay.
+
+// Import required source code
+// Import three.js core
+import * as THREE from "./build/three.module.js";
+// Import pointer lock controls
+import {
+  PointerLockControls
+} from "./src/PointerLockControls.js";
+import { GLTFLoader } from "./src/GLTFLoader.js";
+
+// Establish variables
+let camera, scene, renderer, controls, material;
+let among_us, mesh4, mesh5, mesh6;
+const objects = [];
+let raycaster;
+
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = false;
+
+let prevTime = performance.now();
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
+const vertex = new THREE.Vector3();
+const color = new THREE.Color();
+
+// Initialization and animation function calls
+init();
+animate();
+
+// Initialize the scene
+function init() {
+
+  // Establish the camera
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+  );
+  camera.position.y = 10;
+
+
+  // Define basic scene parameters
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color("rgb(26, 17, 77)");
+
+  scene.fog = new THREE.Fog(0x7985ba, 0, 200);
+
+  // Define scene lighting
+  const light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75);
+  light.position.set(0.5, 1, 0.75);
+  scene.add(light);
+
+  // Define controls
+  controls = new PointerLockControls(camera, document.body);
+
+  // Identify the html divs for the overlays
+  const blocker = document.getElementById("blocker");
+  const instructions = document.getElementById("instructions");
+
+  // Listen for clicks and respond by removing overlays and starting mouse look controls
+  // Listen
+  instructions.addEventListener("click", function() {
+    controls.lock();
+  });
+  // Remove overlays and begin controls on click
+  controls.addEventListener("lock", function() {
+    instructions.style.display = "none";
+    blocker.style.display = "none";
+  });
+  // Restore overlays and stop controls on esc
+  controls.addEventListener("unlock", function() {
+    blocker.style.display = "block";
+    instructions.style.display = "";
+  });
+  // Add controls to scene
+  scene.add(controls.getObject());
+
+  // Define key controls for WASD controls
+  const onKeyDown = function(event) {
+    switch (event.code) {
+      case "ArrowUp":
+      case "KeyW":
+        moveForward = true;
+        break;
+
+      case "ArrowLeft":
+      case "KeyA":
+        moveLeft = true;
+        break;
+
+      case "ArrowDown":
+      case "KeyS":
+        moveBackward = true;
+        break;
+
+      case "ArrowRight":
+      case "KeyD":
+        moveRight = true;
+        break;
+
+      case "Space":
+        if (canJump === true) velocity.y += 350;
+        canJump = false;
+        break;
+    }
+  };
+
+  const onKeyUp = function(event) {
+    switch (event.code) {
+      case "ArrowUp":
+      case "KeyW":
+        moveForward = false;
+        break;
+
+      case "ArrowLeft":
+      case "KeyA":
+        moveLeft = false;
+        break;
+
+      case "ArrowDown":
+      case "KeyS":
+        moveBackward = false;
+        break;
+
+      case "ArrowRight":
+      case "KeyD":
+        moveRight = false;
+        break;
+    }
+  };
+
+  document.addEventListener("keydown", onKeyDown);
+  document.addEventListener("keyup", onKeyUp);
+
+  // Add raycasting for mouse controls
+  raycaster = new THREE.Raycaster(
+    new THREE.Vector3(),
+    new THREE.Vector3(0, -1, 0),
+    0,
+    10
+  );
+
+  // Generate the ground
+  let floorGeometry = new THREE.PlaneGeometry(2000, 2000, 100, 100);
+  floorGeometry.rotateX(-Math.PI / 2);
+
+  // Vertex displacement pattern for ground
+  let position = floorGeometry.attributes.position;
+
+  for (let i = 0, l = position.count; i < l; i++) {
+    vertex.fromBufferAttribute(position, i);
+
+    vertex.x += Math.random() * 20 - 10;
+    vertex.y += Math.random() * 2;
+    vertex.z += Math.random() * 20 - 10;
+
+    position.setXYZ(i, vertex.x, vertex.y, vertex.z);
+  }
+
+  floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
+
+  position = floorGeometry.attributes.position;
+  const colorsFloor = [];
+
+  for (let i = 0, l = position.count; i < l; i++) {
+    // color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.15 + 0.75);
+    color.setRGB(Math.random() * 0.6+ 0.5, 0.3, Math.random() * 0.25 + 0.75);
+    colorsFloor.push(color.r, color.g, color.b);
+  }
+
+  floorGeometry.setAttribute(
+    "color",
+    new THREE.Float32BufferAttribute(colorsFloor, 3)
+  );
+
+  const floorMaterial = new THREE.MeshBasicMaterial({
+    vertexColors: true
+  });
+
+  const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+
+  // Insert completed floor into the scene
+  scene.add(floor);
+
